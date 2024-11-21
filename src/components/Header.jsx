0086fa101a4bab2a66b10585633ba077658a1ad9
Vivek-Dev-Mern <chennuvivekreddy@gmail.com>
2024-11-21 17:22:26 -0500

    Updated index.html and Header component

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,10 +1,9 @@
 import React from "react";
 import "./Header.css";
 const Header = () => {
-<<<<<<< HEAD
   return (
     <header className="header">
-      <div className="logo">Vivek</div>
+      <div className="logo"> Vivek </div>
       <nav>
         <ul>
           <li>
@@ -23,22 +22,6 @@ const Header = () => {
       </nav>
     </header>
   );
-=======
-    return(
-        <header className='header'>
-            <div className='logo'>Vivek Reddy Chennu</div>
-            <nav>
-                <ul>
-                   <li><a href = "#home">Home</a></li>
-                   <li><a href="#about">About</a></li>
-                   <li><a href="#projects">Projects</a></li>
-                   <li><a href="#contact">Contact</a></li> 
-                </ul>
-            </nav>
-        </header>
-
-    );
->>>>>>> a7994e3afb72bcd4257487939f654acc9b3114f0
 };
 
 export default Header;
